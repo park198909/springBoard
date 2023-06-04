26263542ce4013fd06786e92cd17983e72f06887
park198909 <dudrms1123@naver.com>
2023-06-04 18:33:32 +0900

    회원 가입 페이지 작성

--- src/main/java/com/study/entities/BaseEntity.java
+++ src/main/java/com/study/entities/BaseEntity.java
@@ -3,12 +3,13 @@ package com.study.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @MappedSuperclass
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate
