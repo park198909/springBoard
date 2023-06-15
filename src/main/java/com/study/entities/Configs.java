a5e64e978e7604462e1dcf24e40a60010f785e33
park198909 <dudrms1123@naver.com>
2023-06-16 02:04:39 +0900

    0615 작업

---
+++ src/main/java/com/study/entities/Configs.java
@@ -0,0 +1,12 @@
+package com.study.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity @Data
+public class Configs {
+    @Id
+    private String code;
+    @Lob
+    private String value;
+}
