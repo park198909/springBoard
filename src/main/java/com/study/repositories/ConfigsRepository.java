67d99fea366eefb282d9897c363112595559562f
park198909 <dudrms1123@naver.com>
2023-06-16 02:05:11 +0900

    Revert "0615 작업"
    This reverts commit a5e64e978e7604462e1dcf24e40a60010f785e33.

--- src/main/java/com/study/repositories/ConfigsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.repositories;
-
-import com.study.entities.Configs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfigsRepository extends JpaRepository<Configs, String> {
-}
