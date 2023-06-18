6ce19ae5f331772c658d327d367abd951e8b4a3d
park198909 <dudrms1123@naver.com>
2023-06-18 16:00:58 +0900

    게시판 설정 작업 중

--- src/main/java/com/study/commons/MenuDetail.java
+++ src/main/java/com/study/commons/MenuDetail.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class MenuDetail {
+    private String code;
     private String name;
     private String url;
-
 }
--- src/main/java/com/study/controllers/admins/BoardController.java
+++ src/main/java/com/study/controllers/admins/BoardController.java
@@ -2,16 +2,23 @@ package com.study.controllers.admins;
 
 import com.study.commons.MenuDetail;
 import com.study.commons.Menus;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller("AdminBoardController")
 @RequestMapping("/admin/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final HttpServletRequest request;
+
     /**
      * 게시판 목록(게시판 생성)
      *
@@ -19,15 +26,39 @@ public class BoardController {
      */
     @GetMapping
     public String index(Model model) {
-        commonProcess(model);
+        commonProcess(model, "게시판 목록");
 
         return "admin/board/index";
     }
 
-    private void commonProcess(Model model) {
+    /**
+     * 게시판 등록
+     * @return
+     */
+    @GetMapping("/register")
+    public String register(Model model) {
+        commonProcess(model, "게시판 등록");
+        return "admin/board/config";
+    }
+
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable String bId, Model model) {
+        commonProcess(model, "게시판 수정");
+        return "admin/board/config";
+    }
+
+    private void commonProcess(Model model, String title) {
+        String URI = request.getRequestURI();
+
         // 서브 메뉴 처리
+        String subMenuCode = Menus.getSubmenuCode(request);
+        model.addAttribute("subMenuCode", subMenuCode);
+
         List<MenuDetail> submenus = Menus.gets("board");
         model.addAttribute("submenus", submenus);
+
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("title", title);
 
     }
 
